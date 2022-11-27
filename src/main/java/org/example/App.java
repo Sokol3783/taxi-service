6080a7f8ea520a900c9d862094fe480c800497e2
Anton <sokolplus3783@gmail.com>
2022-11-27 18:28:05 +0200

    stage files

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,7 @@
 package org.example;
 
 public class App {
-
-  public static void main(String[] args) {
+ public static void main(String[] args) {
         System.out.println("Hello world!");
   }
 
