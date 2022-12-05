be1bd78e2e7e64a39d3ee7e00d3d2191e76f93d5
Anton <sokolplus3783@gmail.com>
2022-12-05 19:40:10 +0200

    change name columns in initialdb

--- src/main/webapp/WEB-INF/initialdb.sql
+++ src/main/webapp/WEB-INF/initialdb.sql
@@ -19,8 +19,8 @@ GRANT ALL PRIVILEGES ON DATABASE "TAXI" to "TAXIADMIN";
 create TABLE users (
 	user_id SERIAL PRIMARY KEY,
 	password varchar(30),
-	firstName varchar(50),
-	lastName varchar(50),
+	first_name varchar(50),
+	last_name varchar(50),
 	phone varchar(13) unique,
   birthDate varchar(50),
 	email varchar(50),
@@ -30,8 +30,8 @@ create TABLE users (
 create TABLE cars (
 	car_id SERIAL PRIMARY KEY,
 	driver int references users(id),
-	carNumber varchar(30) unique,
-	carName varchar(100),
+	car_number varchar(30) unique,
+	car_name varchar(100),
 	category varchar(15),
 	capacity int
 );
@@ -52,33 +52,33 @@ create TABLE orders (
 	status varchar(20)
 );
 
-INSERT INTO users(firstName, lastName, phone,role, email, password)
+INSERT INTO users(first_name, last_name, phone,role, email, password)
        VALUES('Федор', 'Зотов', '380993785685', 'Administrator', 'examples@goole.com', 'password1');
 
-INSERT INTO users(firstName, lastName, phone,  role, email, password)
+INSERT INTO users(first_name, last_name, phone,  role, email, password)
        VALUES('Матвей', 'Ильин', '380993185685', 'Driver', 'examples1@goole.com', 'password1');
 
-INSERT INTO users(firstName, lastName, phone,  role, email, password)
+INSERT INTO users(first_name, last_name, phone,  role, email, password)
        VALUES('Nicolas', 'Cage', '380993725685', 'Driver', 'examples1@goole.com', 'password1');
 
-INSERT INTO users(firstName, lastName, phone,  role, email, password)
+INSERT INTO users(first_name, last_name, phone,  role, email, password)
     VALUES('IVAN', 'IVANOV', '380993335685', 'User', 'examples2@goole.com', 'password1');
 
-INSERT INTO users(firstName, lastName, phone,  role, email, password)
+INSERT INTO users(first_name, last_name, phone,  role, email, password)
        VALUES('PETER', 'PETROV', '380993335689', 'User', 'examples3@goole.com', 'password1');
 
-INSERT INTO users(firstName, lastName, phone,  role, email, password)
+INSERT INTO users(first_name, last_name, phone,  role, email, password)
        VALUES('Андрей', 'Плеханов', '380999435685', 'User', 'examples4@goole.com', 'password1');
 
-INSERT INTO users(firstName, lastName, phone,  role, email, password)
+INSERT INTO users(first_name, last_name, phone,  role, email, password)
        VALUES('Давид', 'Голиафов', '380993435685', 'User', 'examples5@goole.com', 'password1');
 
-INSERT INTO users(firstName, lastName, phone,  role, email, password)
+INSERT INTO users(first_name, last_name, phone,  role, email, password)
        VALUES('Василий', 'Кузнецов', '380963437885', 'User', 'examples6@goole.com', 'password1');
 
-INSERT INTO cars(driver, carNumber, carName,category, capacity)
+INSERT INTO cars(driver, car_number, car_name,category, capacity)
         VALUES(2, 'AX097A1','Шеврлое Авео', 'Comfort', 4);
 
-INSERT INTO cars(driver, carNumber, carName,category, capacity)
+INSERT INTO cars(driver, car_number, car_name,category, capacity)
 VALUES(3, 'AX807A1','BMW m3', 'Premium', 2);
 
