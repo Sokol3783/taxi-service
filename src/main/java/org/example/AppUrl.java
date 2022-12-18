ae07b91acb36f7bea30d027488d283451937e9b0
Anton <sokolplus3783@gmail.com>
2022-12-18 04:34:33 +0200

    remove not found page

--- src/main/java/org/example/AppUrl.java
+++ src/main/java/org/example/AppUrl.java
@@ -6,7 +6,6 @@ public class AppUrl {
   public static final String USER = "/pages/user.jsp";
   public static final String INDEX = "/index.jsp";
   public static final String DRIVER = "/pages/driver.jsp";
-  public static final String NOT_FOUND = "/pages/notfound.jsp";
   public static final String REGISTRATION = "/pages/registration.jsp";
-  
+
 }
