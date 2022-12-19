928d6a039d62f38dc6009744ad6aab57f2532b5d
Anton <sokolplus3783@gmail.com>
2022-12-19 13:08:51 +0200

    add path to servlets and constants

---
+++ src/main/java/org/example/AppURL.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class AppURL {
+
+  public static final String ADMIN_JSP = "/pages/admin.jsp";
+  public static final String USER_JSP = "/pages/user.jsp";
+  public static final String INDEX_JSP = "/index.jsp";
+  public static final String REGISTRATION_JSP = "/pages/registration.jsp";
+  public static final String USER_SERVLET = "/user";
+  public static final String ADMIN_SERVLET = "/admin";
+}
