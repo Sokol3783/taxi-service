249048eb3fa204c5351bf61235e52dc8b3f7f8ac
Anton <sokolplus3783@gmail.com>
2023-01-21 17:08:26 +0200

    add methods

--- src/main/java/org/example/dao/OrderDAO.java
+++ src/main/java/org/example/dao/OrderDAO.java
@@ -1,7 +1,16 @@
 package org.example.dao;
 
+import org.example.models.Car;
+
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 public interface OrderDAO<T extends Serializable> extends DAO<T> {
 
+    T getByNumber(String number);
+
+    List<T> getByNumbers(List<String> numbers);
+
+    T replaceCar(T model, Map<Car, List<Car>> cars);
 }
