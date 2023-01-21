227ef7833da5538d177542b89458d2c0b2bf56de
Anton <sokolplus3783@gmail.com>
2023-01-21 16:26:42 +0200

    change smth

--- src/main/java/org/example/dao/daoreport/QueryReportTexts.java
+++ src/main/java/org/example/dao/daoreport/QueryReportTexts.java
@@ -1,6 +1,6 @@
-package org.example.dao.daoimplementaion;
+package org.example.dao.daoreport;
 
-public class QueryReportDAO {
+public class QueryReportTexts {
     public final static String reportByPeriod = "SELECT orders.order_number,orders.cost,orders.percent_discount,orders.distance,format('%s %s', u.first_name, u.last_name) as full_name,orders.client_id as user_id FROM orders Left Outer Join users u on u.user_id = orders.client_id WHERE orders.create_date between TO_TIMESTAMP(?, 'YYYY/MM/DD HH:MI:SS')  and TO_TIMESTAMP(?, 'YYYY/MM/DD HH:MI:SS')";
     public final static String reportByPeriodAndClient = reportByPeriod + " AND user_id = ?";
 }
