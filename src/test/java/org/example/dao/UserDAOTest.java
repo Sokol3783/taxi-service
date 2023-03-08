c4c164165760c7babba9caa447271dd20e852d27
Anton <sokolplus3783@gmail.com>
2023-03-09 00:36:27 +0200

    began to write tests to UserDAO

--- src/test/java/org/example/dao/UserDAOTest.java
+++ src/test/java/org/example/dao/UserDAOTest.java
@@ -1,10 +1,23 @@
 package org.example.dao;
 
 import org.example.dao.daoimplementaion.UserDAOimpl;
+import org.example.util.WebAppInitializer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class UserDAOTest {
+
+  @BeforeAll
+  void initializeApp() {
+    WebAppInitializer.initializeApp(WebAppInitializerTest.class);
+  }
+
+  @AfterAll
+  void dropDatabase() {
+    TestUtils.dropDatabase();
+  }
 
   @Test
   void createUser() {
