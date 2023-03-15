5626e805c080873111b6891740d287678d9327f5
Anton <sokolplus3783@gmail.com>
2023-03-15 23:06:49 +0200

    create RandomModels

--- src/test/java/org/example/dao/TestUtils.java
+++ src/test/java/org/example/dao/TestUtils.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.tomcat.jdbc.pool.DataSource;
@@ -16,11 +15,6 @@ import org.example.dao.daoutil.DAOUtil;
 import org.example.util.FileReader;
 
 class TestUtils {
-
-  private static final String[] UKRAINE_CODES = {"50", "63", "66", "67", "68", "73", "91", "92",
-      "93", "94", "95", "96", "97", "98", "99"};
-  private static final String[] DOMAINS = {"gmail.com", "yahoo.com", "hotmail.com", "ukr.net",
-      "i.ua", "bigmir.net", "meta.ua"};
 
   private static DataSource getDefaultPostgresDataSource() {
     DataSource data = new DataSource();
@@ -53,17 +47,5 @@ class TestUtils {
     }
   }
 
-  public static String generatePhoneNumber() {
-    Random random = new Random();
-    String code = UKRAINE_CODES[random.nextInt(UKRAINE_CODES.length)];
-    String number = String.format("%07d", random.nextInt(10_000_000));
-    return "+38" + code + number;
-  }
 
-  public static String generateEmail() {
-    Random random = new Random();
-    String name = "user" + random.nextInt(1000);
-    String domain = DOMAINS[random.nextInt(DOMAINS.length)];
-    return name + "@" + domain;
-  }
 }
