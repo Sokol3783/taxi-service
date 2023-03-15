6a8d11c59931355173c23a7990a587e289e0147f
Anton <sokolplus3783@gmail.com>
2023-03-16 00:08:17 +0200

    add mirror changes to UserTests

--- src/test/java/org/example/dao/UserDAOTest.java
+++ src/test/java/org/example/dao/UserDAOTest.java
@@ -1,21 +1,16 @@
 package org.example.dao;
 
-import static org.example.dao.TestUtils.generateEmail;
-import static org.example.dao.TestUtils.generatePhoneNumber;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import org.example.dao.connectionpool.BasicConnectionPool;
 import org.example.dao.daoimplementaion.UserDAOimpl;
 import org.example.exceptions.DAOException;
 import org.example.models.User;
-import org.example.models.User.UserRole;
 import org.example.security.PasswordAuthentication;
 import org.example.util.WebAppInitializer;
 import org.junit.jupiter.api.AfterAll;
@@ -29,17 +24,17 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 class UserDAOTest {
 
   @BeforeAll
-  static void initializeApp() {
+  static void setUp() {
     WebAppInitializer.initializeApp(WebAppInitializerTest.class);
   }
 
   @AfterAll
-  static void dropDatabase() {
+  static void tearDown() {
     TestUtils.dropDatabase();
   }
 
   @BeforeEach
-  public void setUp() {
+  public void setUpEach() {
     Connection connection = BasicConnectionPool.getInstance().getConnection();
     PreparedStatement statement = null;
     try {
@@ -52,60 +47,44 @@ class UserDAOTest {
 
   @Test
   void createUserAndGetUserById() {
+
     UserDAOimpl userDAO = UserDAOimpl.getInstance();
-    String phone = generatePhoneNumber();
-    String email = generateEmail();
-    String password = "012345789";
-    User user = new User(password, UserRole.USER, "John", "Doe", LocalDate.of(2000, 1, 1),
-        phone, email, 0);
+    User user = RandomModels.generateUser();
     User createdUser = userDAO.create(user);
     User getUser = userDAO.get(user.getId());
     assertEquals(getUser, createdUser);
     assertEquals(user, createdUser);
+
   }
 
   @Test
   void createAndGetAllUsers() {
-    List<User> users = new ArrayList<>();
-    UserDAOimpl userDAO = UserDAOimpl.getInstance();
-    User user = new User("", UserRole.USER, "John", "Doe", LocalDate.of(2000, 1, 1),
-        generatePhoneNumber(), generateEmail(), 0);
-    users.add(userDAO.create(user));
-
-    User user2 = new User("", UserRole.USER, "John", "Doe", LocalDate.of(2000, 1, 1),
-        generatePhoneNumber(), generateEmail(), 0);
-    users.add(userDAO.create(user2));
-
-    User user3 = new User("", UserRole.USER, "John", "Doe", LocalDate.of(2000, 1, 1),
-        generatePhoneNumber(), generateEmail(), 0);
-    users.add(userDAO.create(user3));
-
-    User user4 = new User("", UserRole.USER, "John", "Doe", LocalDate.of(2000, 1, 1),
-        generatePhoneNumber(), generateEmail(), 0);
-    users.add(userDAO.create(user4));
-
-    List<User> all = userDAO.getAll();
-
-    assertEquals(users, all);
+    UserDAO<User> dao = UserDAOimpl.getInstance();
+    List<User> users = List.of(RandomModels.generateUsers(10));
+    for (User user : users) {
+      dao.create(user);
+    }
+    List<User> userFromDB = dao.getAll();
+    assertEquals(users.size(), userFromDB.size());
+    assertEquals(users, userFromDB);
   }
 
   @Test
   void createUserWithPasswordAndGetUserByLogin() {
+
     UserDAOimpl userDAO = UserDAOimpl.getInstance();
-    String password = "0123456789";
-    String phone = generatePhoneNumber();
-    String mail = generateEmail();
-    User user = new User(password, UserRole.USER, "John", "Doe", LocalDate.of(2000, 1, 1),
-        phone, mail, 0);
+
+    User user = RandomModels.generateUser();
+    String password = user.getPassword();
+    
     userDAO.create(user);
     PasswordAuthentication passwordAuthentication = new PasswordAuthentication();
     assertThrows(DAOException.class, () -> userDAO.getUserPhoneMailAndPassword("380997825686",
         passwordAuthentication.hash("0123456789".toCharArray())));
 
-    String phone1 = generatePhoneNumber();
-    String mail1 = generateEmail();
-    User user1 = new User(password, UserRole.USER, "John", "Doe", LocalDate.of(2000, 1, 1),
-        phone1, mail1, 0);
+    User user1 = RandomModels.generateUser();
+    String phone1 = user1.getPhone();
+    String mail1 = user1.getEmail();
 
     String hash = passwordAuthentication.hash(password.toCharArray());
 
@@ -124,8 +103,7 @@ class UserDAOTest {
   void updatePassword() {
     UserDAOimpl userDAO = UserDAOimpl.getInstance();
     String password = "0123456789";
-    User user = new User(password, UserRole.USER, "John", "Doe", LocalDate.of(2000, 1, 1),
-        generatePhoneNumber(), generateEmail(), 0);
+    User user = RandomModels.generateUser();
     User userInDB = userDAO.create(user, password);
     User userPhone = userDAO.getUserPhoneMailAndPassword(user.getPhone(), password);
     assertEquals(userInDB, userPhone);
